41631afeee404d3e2d62c982fa4a3afee1da9d69
jvivek17 <jlakshmivivek17@gmail.com>
2023-11-23 00:30:02 -0500

    updated Get method on client

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 HEADER = 64
 PORT = 5050
@@ -21,3 +22,34 @@ def send(msg):
 
 send("Hello World!")
 input()
+
+def read_data_from_minion():
+    return None
+
+# def get()
+def retrieve_file_data(master, filename, enable_debug=False):
+    file_entry = master.fetch_file_entry(filename)
+
+    if not file_entry:
+        print ("File not found in the list")
+        return
+    
+    for block_info in file_entry:
+        if enable_debug:
+            print("Debug Info:", block_info)
+
+        for minion_id in [master.get_minions()[_] for _ in block_info[1]]:
+            data_chunk = read_data_from_minion(block_info[0], minion_id)
+
+            if data_chunk:
+                sys.stdout.write(data_chunk)
+                break
+        
+        else:
+            print("missing data in file Block")
+
+    
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
